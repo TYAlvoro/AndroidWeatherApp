9e49dbaab19c386bdf5fac0c2b5aad3efb0884fa
AlVoro25 <yahyaev.tamirlan@yandex.ru>
2024-09-12 16:19:28 +0300

    Создан data class для CityResponse

--- app/src/main/java/com/example/weatherapp/api/model/CityResponse.kt
+++ app/src/main/java/com/example/weatherapp/api/model/CityResponse.kt
@@ -1,2 +1,10 @@
 package com.example.weatherapp.api.model
 
+data class CityResponse (
+    val name : String,
+    val localNames: Map<String, String>,
+    val lat: Double,
+    val lon: Double,
+    val country: String,
+    val state: String
+)
