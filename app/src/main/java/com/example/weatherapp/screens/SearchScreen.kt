50ad1515f399caa8fd18a8be86db817c8211af66
AlVoro25 <yahyaev.tamirlan@yandex.ru>
2024-09-12 14:38:19 +0300

    Готовый дизайн строки поиска

--- app/src/main/java/com/example/weatherapp/screens/SearchScreen.kt
+++ app/src/main/java/com/example/weatherapp/screens/SearchScreen.kt
@@ -7,23 +7,36 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 data object SearchScreen : BaseScreen() {
@@ -55,7 +68,7 @@ data object SearchScreen : BaseScreen() {
                 .background(Color(0xfff7f7f7))
                 .padding(paddingValues)
         ) {
-            items(10) { index ->
+            items(0) { index ->
                 ItemCard(index = index)
             }
         }
@@ -85,17 +98,20 @@ data object SearchScreen : BaseScreen() {
     }
 
     @Composable
-    fun SearchBar (
+    fun SearchBar(
         searchQuery: String,
         onQueryChanged: (String) -> Unit,
-        onSearchClicked : () -> Unit
+        onSearchClicked: () -> Unit
     ) {
-        Row (
+        Row(
             modifier = Modifier
+                .padding(top = 30.dp, start = 10.dp, end = 10.dp, bottom = 10.dp)
                 .fillMaxWidth()
-                .background(Color.White)
-                .border(BorderStroke(1.dp, Color.Black))
-                .padding(20.dp)
+                .height(55.dp)
+                .background(Color.White, shape = RoundedCornerShape(20.dp))
+                .border(border = BorderStroke(1.dp, Color(0xffbababa)), shape = RoundedCornerShape(20.dp))
+                .padding(horizontal = 8.dp, vertical = 1.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             TextField(
                 value = searchQuery,
@@ -103,14 +119,46 @@ data object SearchScreen : BaseScreen() {
                 modifier = Modifier
                     .weight(1f)
                     .padding(end = 8.dp),
-                placeholder = { Text(text = "Найти город...") }
+                placeholder = {
+                    Text(
+                        text = "Найти город...",
+                        style = TextStyle(
+                            fontSize = 16.sp, // Размер шрифта плейсхолдера
+                            fontFamily = FontFamily.Serif, // Шрифт плейсхолдера
+                            color = Color.Gray // Цвет плейсхолдера
+                        )
+                    )
+                },
+                singleLine = true,
+                textStyle = TextStyle(
+                    fontSize = 16.sp, // Размер шрифта текста
+                    fontFamily = FontFamily.Serif, // Шрифт текста
+                    color = Color.Black // Цвет текста
+                ),
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    cursorColor = Color.Blue,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent
+                ),
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.Sentences // Каждое предложение с большой буквы
+                )
             )
 
-            Button(
+            IconButton(
                 onClick = onSearchClicked,
-                modifier = Modifier.alignByBaseline()
+                modifier = Modifier.size(40.dp) // Устанавливаем размер кнопки
             ) {
-                Text("Search")
+                Icon(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(end = 4.dp),
+                    imageVector = Icons.Default.Search, // Иконка лупы
+                    contentDescription = "Search",
+                    tint = Color.Black // Цвет иконки
+                )
             }
         }
     }
